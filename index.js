071f6fc5a918578a17a5759496139757fd1b6d05
Dibyajyoti <dibyajyoti@Dibyajyotis-MacBook-Pro.local>
2024-11-04 20:20:38 +0530

    welcome added

--- index.js
+++ index.js
@@ -3,19 +3,29 @@ const app = express();
 const db = require('./db');
 require('dotenv').config();
 
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
 const PORT = process.env.PORT || 3000;
 
 // Import the router files
 const userRoutes = require('./routes/userRoutes');
 const candidateRoutes = require('./routes/candidateRoutes');
+const { default: mongoose } = require('mongoose');
 
 // Use the routers
+app.get("/", (req, res) => res.send("Welcome"))
 app.use('/user', userRoutes);
 app.use('/candidate', candidateRoutes);
 
 
-app.listen(PORT, ()=>{
-    console.log('listening on port 3000');
-})
+mongoose
+    .connect(process.env.MONGODB_URL_LOCAL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("App has started")
+        app.listen(PORT, () => {
+            console.log('listening on port 3000');
+        })
+    })
